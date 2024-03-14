70f28509080086600ef3b34ed2770b8c31bf22e2
ValentinaCifras <valentina.cifras@gmail.com>
2024-03-14 15:50:04 -0300

    desafio trivia

---
+++ desafiodia15/level.py
@@ -0,0 +1,16 @@
+def choose_level(n_pregunta, p_level):
+# Construir lógica para escoger el nivel
+    if n_pregunta <= p_level:
+        level = 'basicas'
+    elif n_pregunta <= p_level*2:
+        level = 'intermedias'
+    else:
+        level = 'avanzadas'
+    return level
+
+if __name__ == '__main__':
+    # verificar resultados
+    print(choose_level(2, 2)) # basicas
+    print(choose_level(3, 2)) # intermedias
+    print(choose_level(7, 2)) # avanzadas
+    print(choose_level(4, 3)) # intermedias
---
+++ desafiodia15/preguntas.py
@@ -0,0 +1,63 @@
+preguntas_basicas = {
+    'pregunta_1': {'enunciado':['Indique la Capital de Chile'],
+    'alternativas': [['Viña del Mar', 0], 
+                     ['Santiago', 1], 
+                     ['La Serena', 0], 
+                     ['Puerto Varas', 0]]},
+    'pregunta_2': {'enunciado':['Indique la Capital de España'],
+    'alternativas': [['Barcelona', 0], 
+                     ['Madrid', 1], 
+                     ['Sevilla', 0], 
+                     ['Toledo', 0]]},
+    
+'pregunta_3': {'enunciado':['Indique la Capital de Estados Unidos'],
+    'alternativas': [['Philadelphia', 0], 
+                     ['Whashinton D.C', 1], 
+                     ['Boston', 0], 
+                     ['New York', 0]]}
+}
+
+
+preguntas_intermedias = {
+    'pregunta_1': {'enunciado':['Indique la Capital de Canada'],
+    'alternativas': [['Vancouver', 0], 
+                     ['Ottawa', 1], 
+                     ['Montreal', 0], 
+                     ['Toronto', 0]]},
+    'pregunta_2': {'enunciado':['Indique la Capital de Australia'],
+    'alternativas': [['Sydney', 0], 
+                     ['Canberra', 1], 
+                     ['Brisbane', 0], 
+                     ['Gold Coast', 0]]},
+    
+'pregunta_3': {'enunciado':['Indique la Capital de Nueva Zelanda'],
+    'alternativas': [['Auckland', 0], 
+                     ['Wellington', 1], 
+                     ['Cristchurch', 0], 
+                     ['Queenstown', 0]]}
+}
+
+preguntas_avanzadas = {
+    'pregunta_1': {'enunciado':['Indique la Capital de Islandia'],
+    'alternativas': [['Keflavik', 0], 
+                     ['Reykjavik', 1], 
+                     ['Grindavik', 0], 
+                     ['Selfoss', 0]]},
+    'pregunta_2': {'enunciado':['Indique la Capital de Irlanda'],
+    'alternativas': [['Galway', 0], 
+                     ['Dublin', 1], 
+                     ['Cork', 0], 
+                     ['Limerick', 0]]},
+    
+'pregunta_3': {'enunciado':['Indique la Capital de Filipinas'],
+    'alternativas': [['Cebú', 0], 
+                     ['Manila', 1], 
+                     ['Butuan', 0], 
+                     ['Puerto Princesa', 0]]}
+}
+
+pool_preguntas = {'basicas': preguntas_basicas,
+                  'intermedias': preguntas_intermedias,
+                  'avanzadas': preguntas_avanzadas}
+
+
